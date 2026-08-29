d4359e314b7cb70bbfcf31a5aaf9360e051dac30
Aravindhan Chandran <82622687+aravindhanchandran@users.noreply.github.com>
2026-08-29 22:58:46 +0530

    Add files via upload

---
+++ Python/Loop/Wh01.py
@@ -0,0 +1,8 @@
+# While Loop — Question 1
+# Write a Python program that prints the numbers 1 to 10 using a while loop.
+
+n = 1.
+while n<=10:
+	print( n)
+	n += 1
+
---
+++ Python/Loop/Wh02.py
@@ -0,0 +1,9 @@
+# While Loop — Question 2
+# Write a Python program that prints the even numbers from 1 to 20 using a while loop.
+
+n = 1.
+
+while n <=20:
+	if n % 2 == 0:
+		print(n)
+	n+=1.
